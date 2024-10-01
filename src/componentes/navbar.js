c6f80f00a9d6849cb8422db801437a849730d89a
Jahir Velazquez <jvelazquez3g2001@gmail.com>
2024-10-01 11:10:08 -0700

    Home

--- src/componentes/navbar.js
+++ src/componentes/navbar.js
@@ -77,32 +77,32 @@ const Navbar = ({ user }) => {
   };
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-body-tertiary">
-      <div className="container-fluid">
-        <button
-          data-mdb-collapse-init
-          className="navbar-toggler"
-          type="button"
-          data-mdb-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <i className="fas fa-bars"></i>
-        </button>
+    <nav className="navbar navbar-expand-lg navbar-light bg-body-tertiary" style={{ height: '60px' }}>
+  <div className="container-fluid">
+    <button
+      data-mdb-collapse-init
+      className="navbar-toggler"
+      type="button"
+      data-mdb-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <i className="fas fa-bars"></i>
+    </button>
 
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <a className="navbar-brand mt-2 mt-lg-0" href="#">
-            <img
-              src="https://mdbcdn.b-cdn.net/img/logo/mdb-transaprent-noshadows.webp"
-              height="15"
-              alt="MDB Logo"
-              loading="lazy"
-            />
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <a className="navbar-brand mt-2 mt-lg-0" href="#">
+        <img
+          src="https://firebasestorage.googleapis.com/v0/b/pirotecniacq.appspot.com/o/productos%2Fsadasd_1705947808901.jpg?alt=media&token=f3e0bdef-282d-4dfc-9cff-622e69f66bef"
+          alt="Logo"
+          loading="lazy"
+          style={{ maxHeight: '70px', objectFit: 'contain' }} 
+        />
           </a>
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <Link className="nav-link" to='pum'>Inicio</Link>
+              <Link className="nav-link" to='/'>Inicio</Link>
             </li>
             <li className="nav-item">
               <Link className="nav-link" to='/productos'>Productos</Link>
